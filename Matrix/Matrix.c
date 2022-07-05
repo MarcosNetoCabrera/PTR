6752318dfda0b5ac41cf78212540d36603d3df8e
vinic <vpms@icomp.ufam.edu.br>
2022-07-05 15:52:17 -0400

    Pequenas atualizações 2

--- Matrix/Matrix.c
+++ Matrix/Matrix.c
@@ -11,7 +11,6 @@ Matrix matrix_constructor(unsigned int lines, unsigned int columns){
     matrix.columns = columns;
     matrix.lines = lines;
     matrix.values = malloc(lines*columns*sizeof (double));
-    matrix.values = NULL;
     return matrix;
 }
 
@@ -60,6 +59,7 @@ Matrix matrix_sum(Matrix matrix_1,Matrix matrix_2){
         return matrix_result;
     }else{
         Matrix matrix_result = matrix_constructor(0,0);
+        matrix_result.values = NULL;
         printf("ERRO: As matrizes passadas possuem dimensioes distintas\n");
         return matrix_result;
     }
@@ -76,6 +76,7 @@ Matrix matrix_sub(Matrix matrix_1,Matrix matrix_2){
         return matrix_result;
     }else{
         Matrix matrix_result = matrix_constructor(0,0);
+        matrix_result.values = NULL;
         printf("ERRO: As matrizes passadas possuem dimensioes distintas\n");
         return matrix_result;
     }
